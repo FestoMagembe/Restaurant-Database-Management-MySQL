d9fcea522547ae1cd31a283b4895ccc8e7f90b9b
festomanolo <108662133+FestoMagembe@users.noreply.github.com>
2024-05-23 19:20:04 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+
+ * Export to PHP Array plugin for PHPMyAdmin
+
+ * @version 5.2.1
+
+ */
+
+
+
+/**
+
+ * Database `manolorestaurant`
+
+ */
+
+
+
+/* `manolorestaurant`.`customers` */
+
+$customers = array(
+
+  array('CustomerID' => '1','FirstName' => 'Festo','LastName' => 'Manolo','Email' => 'Festomanolofm@gmail.com','Address' => '123 Chunya','City' => 'Dar es Salaam','Phone' => '+255 784 953 866','Birthday' => '2002-08-15','FavoriteDish' => 'Ugali Nyama'),
+
+  array('CustomerID' => '2','FirstName' => 'Daudi','LastName' => 'Magembe','Email' => 'magembedo@yahoo.com','Address' => '123 makongolosi','City' => 'Dar Es salaam','Phone' => '+255 784 953 866','Birthday' => '2004-03-20','FavoriteDish' => 'Wali Marage'),
+
+  array('CustomerID' => '3','FirstName' => 'Grace','LastName' => 'Mae','Email' => 'gracema@gmail.com','Address' => '124 Uyole','City' => 'Mbeya','Phone' => '+255 784 953 866','Birthday' => '2004-11-10','FavoriteDish' => 'Ugali kabechi'),
+
+  array('CustomerID' => '4','FirstName' => 'Juma','LastName' => 'Edwin','Email' => 'jumaedu@gmail.com','Address' => '124 Mbalizi','City' => 'Mbeya','Phone' => '+255 784 953 866','Birthday' => '2004-06-15','FavoriteDish' => 'Ugali Mchicha'),
+
+  array('CustomerID' => '5','FirstName' => 'Mark','LastName' => 'Zug','Email' => 'mamrzug@gmail.com','Address' => '128 Isyesye','City' => 'Mbeya','Phone' => '+255 784 953 866','Birthday' => '2000-01-03','FavoriteDish' => 'Ugali Nyama'),
+
+  array('CustomerID' => '6','FirstName' => 'shuku','LastName' => 'rani','Email' => 'shuku@gmail.com','Address' => '136 Mbalari','City' => 'Mbeya','Phone' => '+255 784 953 866','Birthday' => '2003-02-20','FavoriteDish' => 'Pilau'),
+
+  array('CustomerID' => '7','FirstName' => 'Kinji','LastName' => 'Kitile','Email' => 'kingkinji@yahoo.com','Address' => '123 Iringa','City' => 'Iringa','Phone' => '+255 784 953 866','Birthday' => '1871-12-10','FavoriteDish' => 'umbwa'),
+
+  array('CustomerID' => '8','FirstName' => 'Rowan','LastName' => 'Nolo','Email' => 'rowwannolo@x.com','Address' => '1338 Road','City' => 'New York City','Phone' => '+255 784 953 866','Birthday' => '1992-06-08','FavoriteDish' => 'Cheeze burger'),
+
+  array('CustomerID' => '9','FirstName' => 'Arnold','LastName' => 'Mwasha','Email' => 'anroldmwasha@yahoo.com','Address' => '456 Mbozi','City' => 'Mbozi','Phone' => '+255 715 112233','Birthday' => '2009-04-13','FavoriteDish' => 'Burger'),
+
+  array('CustomerID' => '10','FirstName' => 'Ali','LastName' => 'said','Email' => 'alisadid@email.com','Address' => '218 Mbezi','City' => 'Dar Es salaam','Phone' => '+255 715 112233','Birthday' => '2006-10-05','FavoriteDish' => 'Bruger'),
+
+  array('CustomerID' => '11','FirstName' => 'Madam','LastName' => 'Maratwe','Email' => 'maratwe@yahoo.com','Address' => '198 Uswahilini','City' => 'Mtwara','Phone' => '+255 715 112233','Birthday' => '1978-02-12','FavoriteDish' => 'Ugali Chaiese'),
+
+  array('CustomerID' => '12','FirstName' => 'Berlie','LastName' => 'Alan','Email' => 'belishalllan.com','Address' => '629 Andromeda','City' => 'Andromeda','Phone' => '+255 715 112233','Birthday' => '2081-07-24','FavoriteDish' => 'pepper'),
+
+  array('CustomerID' => '13','FirstName' => 'Eli','LastName' => 'Eliud','Email' => 'eliahadi@gmail.com','Address' => '24 Mbozi','City' => 'Njombe','Phone' => '+255 715 112233','Birthday' => '2000-11-29','FavoriteDish' => 'wali Mchicha'),
+
+  array('CustomerID' => '14','FirstName' => 'zakayo','LastName' => 'Mfupi','Email' => 'zakayoshort@gmail.com','Address' => '378 Songs','City' => 'Songea','Phone' => '+255 715 112233','Birthday' => '2004-11-27','FavoriteDish' => 'Tambi'),
+
+  array('CustomerID' => '15','FirstName' => 'Rosalia','LastName' => 'Mwidege','Email' => 'rosamwidege@gmail.com','Address' => '272 princeton','City' => 'Los Angeles','Phone' => '+255 715 112233','Birthday' => '2007-09-10','FavoriteDish' => 'wali mkavu'),
+
+  array('CustomerID' => '16','FirstName' => 'Osca','LastName' => 'mengere','Email' => 'oscamengere.com','Address' => '383 bagamoyo','City' => 'Kibaha','Phone' => '+255 715 112233','Birthday' => '1995-05-25','FavoriteDish' => 'Mtori'),
+
+  array('CustomerID' => '17','FirstName' => 'Enya','LastName' => 'Nolo','Email' => 'enyanolo@gmail.com','Address' => '284 Rock City','City' => 'Mwanza','Phone' => '+255 715 112233','Birthday' => '2004-07-07','FavoriteDish' => 'Vitumbua'),
+
+  array('CustomerID' => '18','FirstName' => 'Rich','LastName' => 'Ally ','Email' => 'allyrich@sanaa.com','Address' => '234 Soweto','City' => 'Mbeya','Phone' => '+255 715 112233','Birthday' => '2001-02-01','FavoriteDish' => 'Chai kavu'),
+
+  array('CustomerID' => '19','FirstName' => 'Beatrice','LastName' => 'Lufingo','Email' => 'lufingobeat@gmail.com','Address' => '365 Mwambene','City' => 'IMbeya','Phone' => '+255 715 112233','Birthday' => '1977-06-15','FavoriteDish' => 'Mkorogo'),
+
+  array('CustomerID' => '20','FirstName' => 'Ellen','LastName' => 'Maguzu','Email' => 'ellenmaguzu@gmail.com','Address' => '654 Nanenane','City' => 'Dodoma','Phone' => '+255 715 112233','Birthday' => '2024-05-13','FavoriteDish' => 'Kitimoto'),
+
+  array('CustomerID' => '21','FirstName' => 'Ableli','LastName' => 'simwaba','Email' => 'abelisims@gmail.com','Address' => '128 majengo','City' => 'Mbeya','Phone' => '+255 715 112233','Birthday' => '2006-10-05','FavoriteDish' => 'Wali Nyama')
+
+);
+
+
+
+/* `manolorestaurant`.`dishes` */
+
+$dishes = array(
+
+  array('DishID' => '1','Quality' => 'High','DishDescription' => 'Ugali Nyama','CustomerID' => '1'),
+
+  array('DishID' => '2','Quality' => 'Medium','DishDescription' => 'Pilau Kavu','CustomerID' => '2'),
+
+  array('DishID' => '3','Quality' => 'Low','DishDescription' => 'Ugali Mchicha','CustomerID' => '3'),
+
+  array('DishID' => '4','Quality' => 'High','DishDescription' => 'Ugali Chinese','CustomerID' => '4'),
+
+  array('DishID' => '5','Quality' => 'Medium','DishDescription' => 'Cheeseburger ','CustomerID' => '5'),
+
+  array('DishID' => '6','Quality' => 'Low','DishDescription' => 'Burger','CustomerID' => '6'),
+
+  array('DishID' => '7','Quality' => 'High','DishDescription' => 'Cocktail','CustomerID' => '7'),
+
+  array('DishID' => '8','Quality' => 'Medium','DishDescription' => 'Vitumbua','CustomerID' => '8'),
+
+  array('DishID' => '9','Quality' => 'Low','DishDescription' => 'Chai Mandazi','CustomerID' => '9'),
+
+  array('DishID' => '10','Quality' => 'High','DishDescription' => 'Biriani','CustomerID' => '10'),
+
+  array('DishID' => '11','Quality' => 'Medium','DishDescription' => 'Ndizi Nyama','CustomerID' => '11'),
+
+  array('DishID' => '12','Quality' => 'Low','DishDescription' => 'Biryani','CustomerID' => '12'),
+
+  array('DishID' => '13','Quality' => 'High','DishDescription' => 'Maharage Ya Nazi','CustomerID' => '13'),
+
+  array('DishID' => '14','Quality' => 'Medium','DishDescription' => 'Mchuzi Wa Samaki','CustomerID' => '14'),
+
+  array('DishID' => '15','Quality' => 'Low','DishDescription' => 'Wali Na Kuku','CustomerID' => '15'),
+
+  array('DishID' => '16','Quality' => 'High','DishDescription' => 'Supu Ya Ndizi','CustomerID' => '16'),
+
+  array('DishID' => '17','Quality' => 'Medium','DishDescription' => 'Kisamvu','CustomerID' => '17'),
+
+  array('DishID' => '18','Quality' => 'Low','DishDescription' => 'Mandazi','CustomerID' => '18'),
+
+  array('DishID' => '19','Quality' => 'High','DishDescription' => 'Nyama Ya Nguruwe','CustomerID' => '19'),
+
+  array('DishID' => '20','Quality' => 'Medium','DishDescription' => 'Kitumbua','CustomerID' => '20')
+
+);
+
+
+
+/* `manolorestaurant`.`dishesorders` */
+
+$dishesorders = array(
+
+  array('DishesOrdersID' => '1','DishID' => '1','OrderID' => '1'),
+
+  array('DishesOrdersID' => '2','DishID' => '2','OrderID' => '2'),
+
+  array('DishesOrdersID' => '3','DishID' => '3','OrderID' => '3'),
+
+  array('DishesOrdersID' => '4','DishID' => '4','OrderID' => '4'),
+
+  array('DishesOrdersID' => '5','DishID' => '5','OrderID' => '5'),
+
+  array('DishesOrdersID' => '6','DishID' => '6','OrderID' => '6'),
+
+  array('DishesOrdersID' => '7','DishID' => '7','OrderID' => '7'),
+
+  array('DishesOrdersID' => '8','DishID' => '8','OrderID' => '8'),
+
+  array('DishesOrdersID' => '9','DishID' => '9','OrderID' => '9'),
+
+  array('DishesOrdersID' => '10','DishID' => '10','OrderID' => '10'),
+
+  array('DishesOrdersID' => '11','DishID' => '11','OrderID' => '11'),
+
+  array('DishesOrdersID' => '12','DishID' => '12','OrderID' => '12'),
+
+  array('DishesOrdersID' => '13','DishID' => '13','OrderID' => '13'),
+
+  array('DishesOrdersID' => '14','DishID' => '14','OrderID' => '14'),
+
+  array('DishesOrdersID' => '15','DishID' => '15','OrderID' => '15'),
+
+  array('DishesOrdersID' => '16','DishID' => '16','OrderID' => '16'),
+
+  array('DishesOrdersID' => '17','DishID' => '17','OrderID' => '17'),
+
+  array('DishesOrdersID' => '18','DishID' => '18','OrderID' => '18'),
+
+  array('DishesOrdersID' => '19','DishID' => '19','OrderID' => '19'),
+
+  array('DishesOrdersID' => '20','DishID' => '20','OrderID' => '20')
+
+);
+
+
+
+/* `manolorestaurant`.`orders` */
+
+$orders = array(
+
+  array('OrderID' => '1','CustomerID' => '1','OrderDate' => '2024-01-01'),
+
+  array('OrderID' => '2','CustomerID' => '2','OrderDate' => '2024-01-02'),
+
+  array('OrderID' => '3','CustomerID' => '3','OrderDate' => '2024-01-03'),
+
+  array('OrderID' => '4','CustomerID' => '4','OrderDate' => '2024-01-04'),
+
+  array('OrderID' => '5','CustomerID' => '5','OrderDate' => '2024-01-05'),
+
+  array('OrderID' => '6','CustomerID' => '6','OrderDate' => '2024-01-06'),
+
+  array('OrderID' => '7','CustomerID' => '7','OrderDate' => '2024-01-07'),
+
+  array('OrderID' => '8','CustomerID' => '8','OrderDate' => '2024-01-08'),
+
+  array('OrderID' => '9','CustomerID' => '9','OrderDate' => '2024-01-09'),
+
+  array('OrderID' => '10','CustomerID' => '10','OrderDate' => '2024-01-10'),
+
+  array('OrderID' => '11','CustomerID' => '11','OrderDate' => '2024-01-11'),
+
+  array('OrderID' => '12','CustomerID' => '12','OrderDate' => '2024-01-12'),
+
+  array('OrderID' => '13','CustomerID' => '13','OrderDate' => '2024-01-13'),
+
+  array('OrderID' => '14','CustomerID' => '14','OrderDate' => '2024-01-14'),
+
+  array('OrderID' => '15','CustomerID' => '15','OrderDate' => '2024-01-15'),
+
+  array('OrderID' => '16','CustomerID' => '16','OrderDate' => '2024-01-16'),
+
+  array('OrderID' => '17','CustomerID' => '17','OrderDate' => '2024-01-17'),
+
+  array('OrderID' => '18','CustomerID' => '18','OrderDate' => '2024-01-18'),
+
+  array('OrderID' => '19','CustomerID' => '19','OrderDate' => '2024-01-19'),
+
+  array('OrderID' => '20','CustomerID' => '20','OrderDate' => '2024-01-20')
+
+);
+
